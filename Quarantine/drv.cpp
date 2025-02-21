27bf6f7b2beb7f227133d14958e821061e19cb8b
Yarden <adiyrad@bgu.post.ac.il>
2025-02-21 18:33:41 +0200

    Hiding folder

---
+++ Quarantine/drv.cpp
@@ -0,0 +1,662 @@
+#include "drv.h"
+
+EXECUTIVE_RESOURCE g_Lock;
+PFLT_FILTER g_pFilter;
+PDRIVER_OBJECT g_pDriverObject;
+PUNICODE_STRING g_pQuarantineDirPath;
+
+NTSTATUS
+CompleteRequest(
+	PIRP Irp,
+	NTSTATUS Status, // Default status for IRP
+	ULONG_PTR Info				  // Default info which is used for (written usually)
+)
+{
+	Irp->IoStatus.Status = Status;
+	Irp->IoStatus.Information = Info;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	return Status;
+}
+
+extern "C"
+NTSTATUS
+DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	LOG("Enter");
+
+	NTSTATUS Status = STATUS_SUCCESS;
+	PDEVICE_OBJECT DeviceObj = NULL;
+	UNICODE_STRING Symlink = RTL_CONSTANT_STRING(L"\\??\\Quarantine");
+	BOOLEAN SymlinkCreated = FALSE;
+	g_pFilter = NULL;
+	g_pQuarantineDirPath = NULL;
+
+
+	do {
+
+		// Init minifilter
+		Status = InitMiniFilter(DriverObject, RegistryPath);
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("Failed to init minifilter (0x%u)", Status);
+			break;
+		}
+		
+		// Create device
+		UNICODE_STRING DeviceName = RTL_CONSTANT_STRING(L"\\Device\\Quarantine");
+		Status = IoCreateDevice(
+			DriverObject,
+			0,
+			&DeviceName,
+			FILE_DEVICE_UNKNOWN,
+			0,
+			FALSE,
+			&DeviceObj);
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("Failed to create device (0x%u)", Status);
+			break;
+		}
+
+		// Create symlink
+		Status = IoCreateSymbolicLink(&Symlink, &DeviceName);
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("Failed to symboliclink (0x%u)", Status);
+			break;
+		}
+		else {
+			SymlinkCreated = TRUE;
+		}
+
+		g_pQuarantineDirPath = (PUNICODE_STRING)ExAllocatePoolWithTag(NonPagedPool, MAX_PATH * sizeof(WCHAR) + sizeof(UNICODE_STRING), DRIVER_TAG);
+		if (!g_pQuarantineDirPath)
+		{
+			Status = STATUS_MEMORY_NOT_ALLOCATED;
+			LOG("Failed to allocate memory for quarantine dir");
+			break;
+		}
+
+		RtlInitUnicodeString(g_pQuarantineDirPath, QuarantineDirPath);
+
+		// Start filtering
+		Status = FltStartFiltering(g_pFilter);
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("Failed start filtering");
+			break;
+		}
+
+
+
+
+	} while (false);
+
+	if (!NT_SUCCESS(Status))
+	{
+		LOG("Error in DriverEntry: 0x%u", Status);
+
+		if (g_pFilter)
+			FltUnregisterFilter(g_pFilter);
+		if (SymlinkCreated)
+			IoDeleteSymbolicLink(&Symlink);
+		if (DeviceObj)
+			IoDeleteDevice(DeviceObj);
+
+		return Status;
+	}
+
+	// Add the relevant irp major functions
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = OnCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = OnCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = OnDeviceControl;
+
+	g_pDriverObject = DriverObject;
+
+	LOG("DriverEntry success");
+
+	return Status;
+}
+
+NTSTATUS
+OnCreateClose(
+	_In_ PDEVICE_OBJECT,
+	_In_ PIRP Irp
+)
+{
+	LOG("Enter");
+	return CompleteRequest(Irp);
+}
+
+NTSTATUS
+OnDeviceControl(
+	_In_ PDEVICE_OBJECT,
+	_In_ PIRP Irp
+)
+{
+	LOG("Enter");
+	// TODO Handle commands from agent for quarantine
+	return CompleteRequest(Irp);
+}
+
+NTSTATUS
+InitMiniFilter(
+	PDRIVER_OBJECT pDriverObject,
+	PUNICODE_STRING RegistryPath
+)
+{
+	HANDLE hKey = NULL;
+	HANDLE hSubKey = NULL;
+	NTSTATUS Status = STATUS_SUCCESS;
+
+	LOG("Initializing the minifilter");
+
+	do {
+
+		// Add registry data for minifilter registration
+
+		// Open key for write
+		OBJECT_ATTRIBUTES KeyAttr = RTL_CONSTANT_OBJECT_ATTRIBUTES(RegistryPath, OBJ_KERNEL_HANDLE);
+		Status = ZwOpenKey(
+			&hKey,
+			KEY_WRITE,
+			&KeyAttr);
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("ZwOpenKey::Failed open key (0x%u)", Status);
+			break;
+		}
+
+		// Create the subkey and key
+		UNICODE_STRING SubKey = RTL_CONSTANT_STRING(L"Instances");
+		OBJECT_ATTRIBUTES SubKeyAttr;
+		InitializeObjectAttributes(
+			&SubKeyAttr,
+			&SubKey, 
+			OBJ_KERNEL_HANDLE, 
+			hKey, 
+			NULL);
+		Status = ZwCreateKey(
+			&hSubKey,
+			KEY_WRITE,
+			&SubKeyAttr,
+			0,
+			NULL,
+			0,
+			NULL);
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("ZwCreateKey::Failed open subkey (0x%u)", Status);
+			break;
+		}
+
+		// Set the default instance value
+		UNICODE_STRING ValueName = RTL_CONSTANT_STRING(L"DefaultInstance");
+		WCHAR Name[] = L"QuarantineMfltDefaultInstance";
+		Status = ZwSetValueKey(
+			hSubKey,
+			&ValueName,
+			0,
+			REG_SZ,
+			Name,
+			sizeof(Name));
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("ZwSetValueKey::Failed setting default instance name (0x%u)", Status);
+			break;
+		}
+
+		// Create "instance" key under "Instances"
+		UNICODE_STRING InstKeyName;
+		RtlInitUnicodeString(&InstKeyName, Name);
+		HANDLE hInstKey;
+		InitializeObjectAttributes(
+			&SubKeyAttr,
+			&InstKeyName,
+			OBJ_KERNEL_HANDLE,
+			hSubKey,
+			NULL);
+		Status = ZwCreateKey(
+			&hInstKey,
+			KEY_WRITE,
+			&SubKeyAttr,
+			0,
+			NULL,
+			0,
+			NULL
+		);
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("ZwCreateKey::Failed creating instance key (0x%u)", Status);
+			break;
+		}
+
+		// Write altitude
+		WCHAR Altitude[] = L"409898";
+		UNICODE_STRING AltitudeName = RTL_CONSTANT_STRING(L"Altitude");
+		Status = ZwSetValueKey(
+			hInstKey,
+			&AltitudeName,
+			0,
+			REG_SZ,
+			Altitude,
+			sizeof(Altitude));
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("ZwSetValueKey::Failed writing altitude (0x%u)", Status);
+			break;
+		}
+
+		// Write flags
+		UNICODE_STRING FlagName = RTL_CONSTANT_STRING(L"Flags");
+		ULONG Flags = 0;
+		Status = ZwSetValueKey(
+			hInstKey,
+			&FlagName,
+			0,
+			REG_DWORD,
+			&Flags,
+			sizeof(Flags));
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("ZwSetValueKey::Failed writing flags (0x%u)", Status);
+			break;
+		}
+
+		ZwClose(hInstKey);
+
+		// Construct the callbacks array
+		FLT_OPERATION_REGISTRATION CONST Callbacks[] =
+		{
+			{IRP_MJ_DIRECTORY_CONTROL, 0, OnPreDirectoryControl, OnPostDirectoryControl},
+			{IRP_MJ_OPERATION_END}
+		};
+
+		// Construct the registration struct
+		FLT_REGISTRATION CONST Reg =
+		{
+			sizeof(FLT_REGISTRATION),
+			FLT_REGISTRATION_VERSION,
+			0,										// Flags
+			NULL,									// Context
+			Callbacks,								// Operation callbacks
+			QuarantineUnload,						// Unload
+			QuarantineInstanceSetup,				// Instance setup
+			QuarantineInstanceQueryTeardown,		// Instance query teardown
+			QuarantineInstanceTeardownStart,		// Instance teardown start
+			QuarantineInstanceTeardownComplete,		// Instance teardown complete
+		};
+
+		Status = FltRegisterFilter(pDriverObject, &Reg, &g_pFilter);
+		if (!NT_SUCCESS(Status))
+		{
+			LOG("FltRegisterFilter::Failed FltRegisterFilter (0x%u)", Status);
+			break;
+		}
+
+	} while (false);
+
+	// If subkey not NULL
+	if (hSubKey)
+	{
+		// If we failed in the process
+		if (!NT_SUCCESS(Status))
+		{
+			ZwDeleteKey(hSubKey);
+		}
+	}
+
+	// If key is not null
+	if (hKey)
+	{
+		ZwClose(hKey);
+	}
+
+	return Status;
+}
+
+NTSTATUS
+QuarantineUnload(
+	_In_ FLT_FILTER_UNLOAD_FLAGS Flags
+)
+{
+	UNREFERENCED_PARAMETER(Flags);
+	LOG("Unloading quarantine filter");
+	FltUnregisterFilter(g_pFilter);
+	UNICODE_STRING Symlink = RTL_CONSTANT_STRING(L"\\??\\Quarantine");
+	IoDeleteSymbolicLink(&Symlink);
+	IoDeleteDevice(g_pDriverObject->DeviceObject);
+	ExFreePool(g_pQuarantineDirPath);
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS
+QuarantineInstanceSetup(
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ FLT_INSTANCE_SETUP_FLAGS Flags,
+	_In_ DEVICE_TYPE VolumeDeviceType,
+	_In_ FLT_FILESYSTEM_TYPE VolumeFilesystemType
+)
+{
+	UNREFERENCED_PARAMETER(FltObjects);
+	UNREFERENCED_PARAMETER(Flags);
+	UNREFERENCED_PARAMETER(VolumeDeviceType);
+
+	LOG("Enter");
+
+	NTSTATUS Status = STATUS_SUCCESS;
+
+	if (VolumeFilesystemType != FLT_FSTYPE_NTFS)
+		Status = STATUS_FLT_DO_NOT_ATTACH;
+
+	return Status;
+}
+
+NTSTATUS
+QuarantineInstanceQueryTeardown(
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ FLT_INSTANCE_QUERY_TEARDOWN_FLAGS Flags
+)
+{
+	UNREFERENCED_PARAMETER(FltObjects);
+	UNREFERENCED_PARAMETER(Flags);
+
+	LOG("Enter");
+
+	return STATUS_SUCCESS;
+}
+
+VOID
+QuarantineInstanceTeardownStart(
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ FLT_INSTANCE_TEARDOWN_FLAGS Flags
+)
+{
+	UNREFERENCED_PARAMETER(FltObjects);
+	UNREFERENCED_PARAMETER(Flags);
+
+	LOG("Enter");
+}
+
+VOID
+QuarantineInstanceTeardownComplete(
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ FLT_INSTANCE_TEARDOWN_FLAGS Flags
+)
+{
+	UNREFERENCED_PARAMETER(FltObjects);
+	UNREFERENCED_PARAMETER(Flags);
+
+	LOG("Enter");
+}
+
+
+// On the PostDirectoryControl we can catch
+// i/o requests to see the content of the **folder that contains**
+// our quarantine folder and the hide our folder from the response
+// so the end requester wont know this dir exists
+FLT_POSTOP_CALLBACK_STATUS
+OnPostDirectoryControl(
+	PFLT_CALLBACK_DATA Data,
+	PCFLT_RELATED_OBJECTS FltObjects,
+	PVOID,
+	FLT_POST_OPERATION_FLAGS Flags
+)
+{
+	UNREFERENCED_PARAMETER(FltObjects);
+	LOG("Enter");
+	if (Data->RequestorMode == KernelMode ||
+		Data->Iopb->MinorFunction != IRP_MN_QUERY_DIRECTORY ||
+		(Flags & FLTFL_POST_OPERATION_DRAINING) //this post-operation
+		//  routine has been called for cleanup processing (drained).  Since this
+		//  instance is going away, you should perform a minimum of operations
+		//  while processing this completion.
+		)
+		return FLT_POSTOP_FINISHED_PROCESSING;
+
+	auto& Params = Data->Iopb->Parameters.DirectoryControl.QueryDirectory;
+
+	if (FltObjects->FileObject->FileName.Length <= 0)
+	{
+		LOG("The file name length is >=0  so its possiable a TAB complition");
+		return FLT_POSTOP_FINISHED_PROCESSING;
+	}
+
+	static CONST FILE_INFORMATION_DEFINITION Definitions[] =
+	{
+			FileFullDirectoryInformationDefinition,
+			FileBothDirectoryInformationDefinition,
+			FileDirectoryInformationDefinition,
+			FileNamesInformationDefinition,
+			FileIdFullDirectoryInformationDefinition,
+			FileIdBothDirectoryInformationDefinition,
+			FileIdExtdDirectoryInformationDefinition,
+			FileIdGlobalTxDirectoryInformationDefinition
+	};
+
+	BOOLEAN FoundMatch = FALSE;
+	const FILE_INFORMATION_DEFINITION* pFID = NULL;
+	for (ULONG Index = 0; Index < sizeof(Definitions); Index++)
+	{
+		if (Definitions[Index].Class == Params.FileInformationClass)
+		{
+			pFID = &Definitions[Index];
+			FoundMatch = TRUE;
+		}
+	}
+
+	if (!FoundMatch)
+	{
+		LOG("No match for info class wont operate on the I/O request.");
+		return FLT_POSTOP_FINISHED_PROCESSING;
+	}
+
+	// We want to receive the DOS path of the directory
+	// to compare with the parent directory of our quarantine dir
+	POBJECT_NAME_INFORMATION pDosPath = NULL;
+	IoQueryFileDosDeviceName(FltObjects->FileObject, &pDosPath);
+	if (pDosPath)
+	{
+
+		LOG("Post Querying: '%wZ',  Our quarantine dir: '%wZ'", pDosPath->Name, *g_pQuarantineDirPath);		
+		PUCHAR Base = NULL;
+		
+		// If MDL is available we should use it to retrieve the path
+		if (Params.MdlAddress)
+			Base = (PUCHAR)MmGetSystemAddressForMdlSafe(Params.MdlAddress, NormalPagePriority);
+
+		// If no MDL available we should get the path from the buffer
+		if (!Base)
+			Base = (PUCHAR)Params.DirectoryBuffer;
+
+		// If Base is still NULL we cant compare and we should finish
+		if (Base == NULL)
+			return FLT_POSTOP_FINISHED_PROCESSING;
+
+		// Find the last backslash
+		PWCHAR LastBackSlash = wcsrchr(g_pQuarantineDirPath->Buffer, L'\\');
+		
+		UNICODE_STRING ParentDir;
+		ParentDir.Buffer = g_pQuarantineDirPath->Buffer;
+		// Calculate the length of the full parent directory path
+		// C:\x\y\z\QuarantineDir
+	    // ^       ^
+		//Start    LastBackslash
+		// 0x3     0xB
+		// 0xB - 0x3 = 0x8 (11 - 3 = 8) 
+		// We need to add 1 to include the last backslash too
+		// Then we get the length of: 'C:\x\y\z\' Which is the parent directory
+		// of the QuarantineDir
+		// This way 'ParentDir' is basically the path of the parent directory of 
+		// The quarantine dir
+		ParentDir.Length = USHORT(LastBackSlash - g_pQuarantineDirPath->Buffer) * sizeof(WCHAR);
+
+		LOG("Parent dir of our quarantine dir: %wZ, The current dir: %wZ", ParentDir, pDosPath->Name);
+
+		if (RtlEqualUnicodeString(&ParentDir, &pDosPath->Name, TRUE))
+		{
+			LOG("There is a match IO request for our parent dir");
+
+			// We got an IO request that will reveal our hidden quarantine directory
+			// We should hide it
+			ULONG NextOffset = 0;
+			PUCHAR Prev = NULL;
+			PWCHAR QuarantineDirName = LastBackSlash + 1;
+
+			do {
+
+				// Bug in Windows macro in which FILE_INFORMATION_DEFINITION
+				// File name points to length and vice versa
+				ULONG FileNameLength = *(PULONG)(Base + pFID->FileNameLengthOffset);
+				PCWSTR FileName = (PCWSTR)(Base + pFID->FileNameOffset);
+
+				if (FileNameLength == 0)
+				{
+					// If filename length is 0 this entry is result of Tab completion and we should just skeep this try
+					ExFreePool(pDosPath);
+					Data->IoStatus.Status = STATUS_NOT_FOUND;
+					Data->IoStatus.Information = 0;
+					return FLT_POSTOP_FINISHED_PROCESSING;
+				}
+
+				// Get pointer to the next entry
+				NextOffset = *(PULONG)(Base + pFID->NextEntryOffset);
+
+
+				UNICODE_STRING FileNameDebug;
+				RtlInitUnicodeString(&FileNameDebug, FileName);
+				LOG("Current entry: %wZ", FileNameDebug);
+
+				// If the current entry is our directory we want to hide it form the list
+				if (FileNameLength && _wcsnicmp(QuarantineDirName, FileName, FileNameLength / sizeof(WCHAR)) == 0)
+				{
+					LOG("We found our entry");
+					//First entry, just move the buffer pointer to the next entry
+					if (Prev == NULL)
+					{
+						LOG("Prev is NULL");
+						Params.DirectoryBuffer = Base + NextOffset;
+						FltSetCallbackDataDirty(Data);
+					}
+					else {
+						LOG("Update prev to point to the next offset");
+						// Not the first entry, we should just update the prev entry to point
+						// to the current next entry
+						//PUCHAR TheNextPrevBefore = Prev + *(PULONG)(Prev + pFID->NextEntryOffset);
+						if (NextOffset == 0)
+						{
+							// The entry is the last one we should SET the prev nextEntry to 0
+							LOG("Our directory is the last one");
+							*(PULONG)(Prev + pFID->NextEntryOffset) = 0;
+						}
+						else {
+							// nextEntry none zero we should ADD the current nextOffset
+							LOG("Setting the prev nextOffset");
+							*(PULONG)(Prev + pFID->NextEntryOffset) += NextOffset;
+							//}
+							//PUCHAR TheNextPrevAfter = Prev + *(PULONG)(Prev + pFID->NextEntryOffset);
+
+
+							//// Test it works 
+
+							//PCWSTR FileNameBefore = (PCWSTR)(TheNextPrevBefore + pFID->FileNameOffset);
+							//ULONG FileNameLengthBefore = *(PULONG)(TheNextPrevBefore + pFID->FileNameLengthOffset);
+							//if (FileNameLengthBefore)
+							//{
+							//	UNICODE_STRING FileNameBeforeDebug;
+							//	RtlInitUnicodeString(&FileNameBeforeDebug, FileNameBefore);
+							//	LOG("----The name of prev->next before change: %wZ", FileNameBeforeDebug);
+							//}
+							//else {
+							//	LOG("----The name of prev->next before change: is not existing");
+							//}
+
+							//// Get the name the Prev now points to
+							//PCWSTR FileNameAfter = (PCWSTR)(TheNextPrevAfter + pFID->FileNameOffset);
+							//ULONG FileNameLengthAfter = *(PULONG)(TheNextPrevAfter + pFID->FileNameLengthOffset);
+							//if (FileNameLengthAfter)
+							//{
+							//UNICODE_STRING FileNameAfterDebug;
+							//RtlInitUnicodeString(&FileNameAfterDebug, FileNameAfter);
+							//LOG("----The name of prev->next before change: %wZ", FileNameAfterDebug);
+							//}
+							//else {
+							//	LOG("----The name of prev->next after change: is not existing");
+							//}
+						}
+					}
+
+					break;
+				}
+
+				Prev = Base;
+				Base += NextOffset;
+
+			} while (NextOffset != 0); // When NextOffset it means it is the last entry
+		}
+
+	ExFreePool(pDosPath);
+	}
+	else {
+		LOG("Could not convert to DOS path: '%wZ'", FltObjects->FileObject->FileName);
+	}
+	
+	return FLT_POSTOP_FINISHED_PROCESSING;
+}
+
+// If the I/O request is associating
+// With our quarantine directory
+// We want to deny this request
+FLT_PREOP_CALLBACK_STATUS
+OnPreDirectoryControl(
+	PFLT_CALLBACK_DATA Data,
+	PCFLT_RELATED_OBJECTS FltObjects,
+	PVOID*
+)
+{
+	UNREFERENCED_PARAMETER(FltObjects);
+	LOG("Enter");
+	if (Data->RequestorMode == KernelMode /*|| Data->Iopb->MinorFunction != IRP_MN_QUERY_DIRECTORY*/)
+		return FLT_PREOP_SUCCESS_NO_CALLBACK;
+
+	POBJECT_NAME_INFORMATION NameInfo;
+	if (!NT_SUCCESS(IoQueryFileDosDeviceName(FltObjects->FileObject, &NameInfo)))
+		return FLT_PREOP_SUCCESS_NO_CALLBACK;
+
+	// Test if querying the quarantine directory
+	FLT_PREOP_CALLBACK_STATUS Status = FLT_PREOP_SUCCESS_WITH_CALLBACK;
+	UNICODE_STRING DosPath = NameInfo->Name;
+	LOG("Pre Querying/change: '%wZ',  Our quarantine dir: '%wZ'", DosPath, *g_pQuarantineDirPath);
+	if (RtlEqualUnicodeString(&DosPath, g_pQuarantineDirPath, TRUE))
+	{
+
+		//if (Data->Iopb->MinorFunction == IRP_MN_QUERY_DIRECTORY)
+		//{
+			// Found out that this is an IO req on the quarantine folder
+			LOG("IO request on the Quarantine dir: ('%wZ')", DosPath);
+			Data->IoStatus.Status = STATUS_NOT_FOUND;
+			Data->IoStatus.Information = 0;
+			Status = FLT_PREOP_COMPLETE;
+			goto exit;
+		//}
+		//else if (Data->Iopb->MinorFunction  == IRP_MN_NOTIFY_CHANGE_DIRECTORY)
+		//{
+
+		//	goto exit;
+		//}
+		//else if (Data->Iopb->MinorFunction == IRP_MN_NOTIFY_CHANGE_DIRECTORY_EX)
+		//{
+
+		//	goto exit;
+		//}
+	}
+
+exit:
+	ExFreePool(NameInfo);
+
+	return Status;
+}
---
+++ Quarantine/drv.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <ntifs.h>
+#include <fltKernel.h> // Need to add to the linker: fltmgr.lib
+#include "Utils.h"
+
+#pragma warning(disable: 4995)
+#pragma warning(disable: 4996)
+
+#define DRIVER_TAG 'tnrQ'
+#define DRIVER_PREFIX "QuarantineDrv: "
+
+#define LOG(s, ...) DbgPrint(DRIVER_PREFIX "%s::" s "\n",__FUNCTION__,__VA_ARGS__)
+
+#define MAX_PATH 256
+#define QuarantineDirPath L"C:\\EdrPOC\\Quarantine"
+
+// Declarations
+
+NTSTATUS
+CompleteRequest(
+	PIRP Irp,
+	NTSTATUS Status = STATUS_SUCCESS, // Default status for IRP
+	ULONG_PTR Info = 0				  // Default info which is used for (written usually)
+);				  
+
+NTSTATUS
+OnCreateClose(
+	_In_ PDEVICE_OBJECT DeviceObject,
+	_In_ PIRP Irp
+);
+
+NTSTATUS
+OnDeviceControl(
+	_In_ PDEVICE_OBJECT DeviceObject,
+	_In_ PIRP Irp
+);
+
+NTSTATUS
+InitMiniFilter(
+	PDRIVER_OBJECT  pDriverObject,
+	PUNICODE_STRING RegistryPath
+);
+
+NTSTATUS
+QuarantineUnload(
+	_In_ FLT_FILTER_UNLOAD_FLAGS Flags
+);
+
+NTSTATUS
+QuarantineInstanceSetup(
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ FLT_INSTANCE_SETUP_FLAGS Flags,
+	_In_ DEVICE_TYPE VolumeDeviceType,
+	_In_ FLT_FILESYSTEM_TYPE VolumeFilesystemType
+);
+
+NTSTATUS
+QuarantineInstanceQueryTeardown(
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ FLT_INSTANCE_QUERY_TEARDOWN_FLAGS Flags
+);
+
+VOID
+QuarantineInstanceTeardownStart(
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ FLT_INSTANCE_TEARDOWN_FLAGS Flags
+);
+
+VOID
+QuarantineInstanceTeardownComplete(
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ FLT_INSTANCE_TEARDOWN_FLAGS Flags
+);
+
+FLT_POSTOP_CALLBACK_STATUS
+OnPostDirectoryControl(
+	PFLT_CALLBACK_DATA Data,
+	PCFLT_RELATED_OBJECTS FltObjects,
+	PVOID,
+	FLT_POST_OPERATION_FLAGS Flags
+);
+
+FLT_PREOP_CALLBACK_STATUS
+OnPreDirectoryControl(
+	PFLT_CALLBACK_DATA Data,
+	PCFLT_RELATED_OBJECTS FltObjects,
+	PVOID*
+);
